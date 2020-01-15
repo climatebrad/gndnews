ff93150f7c9df08a3c3f13b2758942bacd850e5c
Brad Johnson <climatebrad@gmail.com>
2020-01-15 15:44:23 -0500

    fix indent

--- topic_modeler/newsifier.py
+++ topic_modeler/newsifier.py
@@ -11,19 +11,19 @@ class NewsifierMixin():
         return self._cluster_names
     
     def top_cluster_predictions(self, idx, num=2):
-    sorted_probs = {
-        k : v for k, v in sorted(
-            { self.cluster_names[i] : x 
-                            for i, x in enumerate(
-                                self.classifier.predict_proba(
-                                    self.vectorized_articles[idx]
-                                )[0]
-                            ) 
-            }.items(),
-            key=lambda item: item[1],
-            reverse=True)
-    }
-    return list(sorted_probs)[:num]
+        sorted_probs = {
+            k : v for k, v in sorted(
+                { self.cluster_names[i] : x 
+                                for i, x in enumerate(
+                                    self.classifier.predict_proba(
+                                        self.vectorized_articles[idx]
+                                    )[0]
+                                ) 
+                }.items(),
+                key=lambda item: item[1],
+                reverse=True)
+        }
+        return list(sorted_probs)[:num]
 
     def top_topics(self, idx, num=3):
         print(self.articles.iloc[idx].title)
