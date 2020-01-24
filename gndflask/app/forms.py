db8d5e7470f58b268026be0d2db9c1ee32eeff6e
Brad Johnson <climatebrad@gmail.com>
2020-01-24 11:56:00 -0500

    remove gndflask

--- gndflask/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class TextForm(FlaskForm):
-    text = TextAreaField('Input Text', render_kw={'class': 'form-control', 'rows': 20})
-    submitbutton = SubmitField('Submit')
